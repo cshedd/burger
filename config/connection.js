2e0d836c331d403d51155533f6a6ee7b1bc3e2e1
Cherie Shedd <cherie.shedd@gmail.com>
2016-08-02 18:10:13 -0500

    database works

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+
+// 		port: 3306,
+// 		host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+// 		user: 'cm0zdmk2ez4igof5',
+// 		password: 'yuhl98jsef0k7ul4',
+// 		database: 'lghowl35ljxl5vao'
+// });
+
 var connection = mysql.createConnection({
 
-		port: 3306,
-		host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user: 'cm0zdmk2ez4igof5',
-		password: 'yuhl98jsef0k7ul4',
-		database: 'lghowl35ljxl5vao'
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
+	password: 'gayle92',
+	database: 'burgers_db'
 });
 
 connection.connect(function(err) {
